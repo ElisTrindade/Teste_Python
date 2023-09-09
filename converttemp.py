c93c82dca7142ded5b94732893977ca19ae4f34e
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 14:13:15 -0300

    Calculadora de conversão de temperatura

---
+++ converttemp.py
@@ -0,0 +1,11 @@
+#  Cálculadora de temperatura =============
+
+tempC = int(input("'Olá, qual a temperatura em graus Celsius hoje?"))
+
+tempF = (1.8*tempC) + 32
+
+tempK = tempC + 273
+
+print(f"A temperatura em Fahrenheit está: {tempF}F")
+print(f"A temperatura em Kelvin está: {tempK}K")
+
