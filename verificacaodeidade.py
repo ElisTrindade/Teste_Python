b525235c64a7c986814997f33fd0f2e20e907402
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 17:32:49 -0300

    Verificaçao de idade

---
+++ verificacaodeidade.py
@@ -0,0 +1,10 @@
+# Verificação de idade ===========================================
+print("Verificação de idade ======================")
+
+idade = float(input("Insira sua idade "))
+maior_idade = 18
+if idade >= maior_idade:
+   print("Você é maior de idade")
+else:
+   print("Você é menor de idade")
+
