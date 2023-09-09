da0ea2ab2c27f64f6f96da0e1141b1b386f6f782
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 17:41:55 -0300

    Loja de roupa

--- lojaderoupa.py
+++ lojaderoupa.py
@@ -1,4 +1,12 @@
 # Uma loja de roupas precisa de um programa que ajude a calcular o valor final da venda de produtos. 
 #Existem algumas regras que precisam ser respeitadas na venda de um produto
 
-# 
+valor_venda = float(input("Insira o valor da venda "))
+tipo_venda = (input("Insira se a venda é a vista ou a prazo "))
+pareclamento = float(input("Em quantas vezes deseja parcela? "))
+
+if (tipo_venda == "a vista" and valor_venda > 500):
+
+elif 
+
+elif (tipo_venda == "a vista" and valor_venda > 1000):
