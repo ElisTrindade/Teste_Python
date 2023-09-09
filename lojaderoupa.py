03803b00a65d16003cac6970682b69ee6b04c2b0
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 17:32:01 -0300

    Loja de roupa

---
+++ lojaderoupa.py
@@ -0,0 +1,4 @@
+# Uma loja de roupas precisa de um programa que ajude a calcular o valor final da venda de produtos. 
+#Existem algumas regras que precisam ser respeitadas na venda de um produto
+
+# 
