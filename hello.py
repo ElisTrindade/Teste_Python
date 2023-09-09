0c36a7004f5a94b1af94be06839528bb9b32aee9
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 10:52:14 -0300

    Criação do arquivo de Hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+nome = "Elis"
+
+print('Hello World!')
+profissao= "Engenheira eletricista e pesquisadora"
+
+print(f"Este é o primeiro programa de {nome}")
+
+print("Eu sou", profissao,"e decidi trilhar os caminhos do Python")
+
+resposta = input("Você está pronto para vir comigo? (S/N)")
