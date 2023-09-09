da2390435e684bf174c1130bcccfdb62e2b90396
Elis Trindade <eliscarolinedesouza@hotmail.com>
2023-09-09 17:32:26 -0300

    Operadores aritméticos

---
+++ operadores.py
@@ -0,0 +1,31 @@
+# Exercicios de operadores lógicos 
+
+
+# Operadores aritméticos
+
+A = 7
+B = 2
+C = 4
+D = 14
+
+quest1 = A + B - C
+quest2 = A * B / C
+quest3 = C + 22 * D
+quest4 = A + B * C + D
+quest5 = (A + B) * (C + D)
+quest6 = (D / A) / (B / C)
+quest7 = 10 ** A
+quest8 = 58 % A
+quest9 = (A + B + C * D) / (B - C * (B - C))
+quest10 = (A + B + C * D) / (B - C * (B - C)) + 10 ** A
+
+print(f"{quest1}")
+print(f"{quest2}")
+print(f"{quest3}")
+print(f"{quest4}")
+print(f"{quest5}")
+print(f"{quest6}")
+print(f"{quest7}")
+print(f"{quest8}")
+print(f"{quest9}")
+print(f"{quest10}")
